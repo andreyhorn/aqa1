6846fa310b324482a7d0cd47f48d199e04d1262c
Andrey Kharlanov <kharlanov.horn@gmail.com>
2025-06-24 15:42:13 +0300

    Add tests TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,46 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturn200at800() {
+        int expected = 200;
+        int actual = service.remain (800);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldReturn1at999() {
+        int expected = 1;
+        int actual = service.remain (999);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldReturn999at1() {
+        int expected = 1;
+        int actual = service.remain (999);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldReturn0at1000() {
+        int expected = 0;
+        int actual = service.remain (1000);
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldReturn999at1001() {
+        int expected = 999;
+        int actual = service.remain (1001);
+
+        assertEquals(actual, expected);
+    }
+
+}
